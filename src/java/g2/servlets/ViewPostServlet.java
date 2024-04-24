ab8416a21194926ef93c0c4af486e5b4c4a6d709
igrrrind <107808162+igrrrind@users.noreply.github.com>
2024-04-24 09:55:21 +0700

    add dependencies to ViewPostServlet.java

--- src/java/g2/servlets/ViewPostServlet.java
+++ src/java/g2/servlets/ViewPostServlet.java
@@ -5,15 +5,24 @@
  */
 package g2.servlets;
 
+import g2.commentTbl.commentDAO;
+import g2.commentTbl.commentDTO;
 import g2.postTbl.postDAO;
 import g2.postTbl.postDTO;
+import g2.userTbl.userDAO;
+import g2.userTbl.userDTO;
+import g2.voteTbl.voteDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 /**
  *
